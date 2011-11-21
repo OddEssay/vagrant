00b9fda1123ff0174ebf77afb51fdbc6667def8c
Paul Freeman <paul@mojo-projects.com>
2011-11-21 15:36:40 +0000

    Changes for BarCampLiv talk

--- manifests/vm.box.pp
+++ manifests/vm.box.pp
@@ -8,15 +8,12 @@ class lucid32
 		path => ["/usr/bin/","/usr/sbin/","/bin"],
 		command => "apt-get update",
 	}
+
  	if ! defined( Package["zlib1g"] )				{ package { "zlib1g": ensure => present, } }
 	if ! defined( Package["zlib1g-dev"] )			{ package { "zlib1g-dev": ensure => present, } }
 	if ! defined( Package["vim"] )					{ package { "vim": ensure => present, } }
 	if ! defined( Package["byobu"] )				{ package { "byobu": ensure => present, } }
 	if ! defined( Package["apache2"] )				{ package { "apache2": ensure => present, } }
-	service { "apache2":
-		ensure => running,
-		require => Package["apache2"],
-	}
 	if ! defined( Package["git-core"] )				{ package { "git-core": ensure => present, } }
 	if ! defined( Package["php-apc"] )				{ package { "php-apc": ensure => present, } }
 	if ! defined( Package["libssl-dev"] )			{ package { "libssl-dev": ensure => present, } }
@@ -25,17 +22,23 @@ class lucid32
 	if ! defined( Package["php-mysql"] )			{ package { "php5-mysql": ensure => present, } }
 	if ! defined( Package["php5-xdebug"] )			{ package { "php5-xdebug": ensure => present, } }
 	if ! defined( Package["memcached"] ) 			{ package { "memcached": ensure => present, } }
+	if ! defined( Package["mysql-server"] )			{ package { "mysql-server": ensure => present, } }
+	if ! defined( Package["curl"] )					{ package { "curl": ensure => present, } }
+
+	service { "apache2":
+		ensure => running,
+		require => Package["apache2"],
+	}
 	service { "memcached":
 		ensure => running,
 		require => Package["memcached"]
 	}
-	if ! defined( Package["mysql-server"] )			{ package { "mysql-server": ensure => present, } }
+	
 	service { "mysql":
 		ensure => running,
 		require => Package["mysql-server"],
 	}
 
-	if ! defined( Package["curl"] )		{ package { "curl": ensure => present, } }
 
 	group { "puppet": 
     	ensure => "present", 
